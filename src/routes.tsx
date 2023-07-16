03a5ccf3b0cd4b0d19e29dfb4cad65c729623979
nxtrm <nextrem_p@proton.me>
2023-07-16 09:07:44 +0300

    handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,18 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import GameDetailsPage from "./pages/GameDetailsPage";
 import Layout from "./pages/Layout";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <ErrorPage />,
     element: <Layout />,
-    children: [{ index: true, element: <HomePage /> }],
-  },
-
-  {
-    path: "/games/:id",
-    element: <Layout />,
-    children: [{ index: true, element: <GameDetailsPage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "/games/:id",
+        element: <GameDetailsPage />,
+      },
+    ],
   },
 ]);
 
