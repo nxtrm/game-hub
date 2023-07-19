b7563a1c09178366e9d0cc1ea9b11c5b0ecd59bc
nxtrm <nextrem_p@proton.me>
2023-07-19 13:53:55 +0300

    Create a dummy payment page

---
+++ src/components/ToolsMenu.tsx
@@ -0,0 +1,45 @@
+import {
+  Box,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuGroup,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { BsPerson } from "react-icons/bs";
+import ColorModeSwitch from "./ColorModeSwitch";
+import { Link } from "react-router-dom";
+
+const ToolsMenu = () => {
+  return (
+    <Menu>
+      <MenuButton as={IconButton} icon={<BsPerson />}>
+        Profile
+      </MenuButton>
+      <MenuList>
+        <MenuGroup title="Profile">
+          <Link to="/account">
+            <MenuItem>My Account</MenuItem>
+          </Link>
+          <Link to="/plans">
+            <MenuItem>Plans </MenuItem>
+          </Link>
+        </MenuGroup>
+
+        <MenuDivider />
+        <MenuGroup title="Misc">
+          <MenuItem>Docs</MenuItem>
+          <MenuItem>FAQ</MenuItem>
+        </MenuGroup>
+        <MenuDivider />
+        <Box paddingX={3}>
+          <ColorModeSwitch />
+        </Box>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default ToolsMenu;
